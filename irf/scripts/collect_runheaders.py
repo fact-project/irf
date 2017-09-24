f945147fc810d973ffce7679a33df9d548b6c319
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-09-24 15:06:19 +0200

    Save more columns and use h5py

--- irf/scripts/collect_runheaders.py
+++ irf/scripts/collect_runheaders.py
@@ -3,10 +3,55 @@ import click
 import os
 import gzip
 import re
-import itertools
 import joblib
+import numpy as np
+import h5py
+
+from fact.io import to_h5py
 
 from ..corsika import read_corsika_headers
+
+event_header_columns = [
+    'run_number',
+    'event_number',
+    'particle_id',
+    'total_energy',
+    'first_interaction_height',
+    'momentum_x',
+    'momentum_y',
+    'momentum_minus_z',
+    'zenith',
+    'azimuth',
+    'n_reuse',
+]
+
+event_header_renames = {
+    'total_energy': 'energy'
+}
+
+event_header_types = {
+    'particle_id': int,
+    'event_number': int,
+    'run_number': int,
+    'n_reuse': int,
+}
+
+run_header_columns = [
+    'run_number',
+    'date',
+    'version',
+    'energy_spectrum_slope',
+    'energy_min',
+    'energy_max',
+    'n_showers',
+    'x_scatter',
+    'y_scatter',
+]
+
+run_header_types = {
+    'run_number': int,
+    'n_showers': int,
+}
 
 
 @click.command()
@@ -43,10 +88,18 @@ def main(directories, outputfile, pattern, n_jobs):
     The output ist stored in the OUTPUTFILE as pandas hdf5 data.
     '''
 
+    with h5py.File(outputfile, 'r+') as f:
+        if 'corsika_runs' in f.keys() or 'corsika_events' in f.keys():
+            click.confirm(
+                'Group "corsika_runs" exists in outputfile, overwrite? ',
+                abort=True,
+            )
+
+            del f['corsika_runs']
+            del f['corsika_events']
+
     filename_re = re.compile(pattern)
     pool = joblib.Parallel(n_jobs=n_jobs, verbose=10)
-
-    results = []
 
     for directory in directories:
         mc_files = filter(filename_re.match, os.listdir(directory))
@@ -56,14 +109,20 @@ def main(directories, outputfile, pattern, n_jobs):
             len(mc_files), directory
         ))
 
-        results += pool(
+        results = pool(
             joblib.delayed(read_mmc_headers)(f)
             for f in mc_files
         )
 
-    df = pd.concat(itertools.chain(results))
-    print('number of events parsed: {}'.format(len(df)))
-    df.to_hdf(outputfile, key='table')
+        event_headers = pd.concat(r[1] for r in results)
+        run_headers = pd.DataFrame([r[0] for r in results])
+        run_headers = run_headers.astype(run_header_types)
+
+        print('number of runs parsed: {}'.format(len(run_headers)))
+        print('number of events parsed: {}'.format(len(event_headers)))
+
+        to_h5py(outputfile, event_headers, key='corsika_events')
+        to_h5py(outputfile, run_headers, key='corsika_runs')
 
 
 def open_file(f):
@@ -78,12 +137,34 @@ def open_file(f):
 def read_mmc_headers(data_file):
 
     with open_file(data_file) as f:
-        headers = read_corsika_headers(f)
+        run_header, event_headers = read_corsika_headers(f)
 
-    energies = [e.total_energy for e in headers['event_headers']]
-    zeniths = [e.zenith_angle for e in headers['event_headers']]
+    event_df = pd.DataFrame(event_headers[event_header_columns])
+    event_df.rename(columns=event_header_renames, inplace=True)
+    event_df = event_df.astype(event_header_types)
+    event_df.set_index('event_number', inplace=True)
 
-    return pd.DataFrame({'energy': energies, 'zenith': zeniths})
+    n_reuse = int(event_headers['n_reuse'][0])
+    assert np.all(n_reuse == event_headers['n_reuse'])
+
+    impact_x = event_headers['reuse_x'][:, :n_reuse].ravel()
+    impact_y = event_headers['reuse_y'][:, :n_reuse].ravel()
+    reuse_id = np.tile(np.arange(n_reuse) + 1, len(event_headers))
+
+    reuse_df = pd.DataFrame({
+        'impact_x': impact_x,
+        'impact_y': impact_y,
+        'reuse_id': reuse_id,
+        'event_number': np.repeat(event_headers['event_number'].astype(int), n_reuse),
+    })
+
+    run_series = pd.Series(
+        dict(zip(
+            run_header[run_header_columns].dtype.names,
+            run_header[run_header_columns][0],
+        ))
+    )
+    return run_series, reuse_df.join(event_df, on=['event_number'])
 
 
 if __name__ == '__main__':
