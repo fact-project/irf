feb8913388a6432a473821cd5d0352afa282aaee
Kai B <kai.bruegge@tu-dortmund.de>
2018-02-13 14:51:52 +0100

    excahnge old style test file

--- irf/tests/test_collection_area.py
+++ irf/tests/test_collection_area.py
@@ -10,6 +10,7 @@ FIXTURE_DIR = os.path.join(
     'test_files',
 )
 
+
 @pytest.fixture
 def showers():
     return fact.io.read_data(os.path.join(FIXTURE_DIR, 'showers.hdf5'), key='table')
@@ -17,27 +18,24 @@ def showers():
 
 @pytest.fixture
 def predictions():
-    return fact.io.read_data(os.path.join(FIXTURE_DIR, 'predictions.hdf5'), key='table')
+    return fact.io.read_data(os.path.join(FIXTURE_DIR, 'gamma_predictions_dl2.hdf5'), key='events')
 
 
 def test_units(showers, predictions):
     assert len(showers) > 0
     assert len(predictions) > 0
-    predictions['energy'] = predictions['MCorsikaEvtHeader.fTotalEnergy']
-    r = collection_area(showers.energy, predictions.energy, bins=10, impact=270*u.m)
-    area  = r[0]
+    predictions['energy'] = predictions['corsika_evt_header_total_energy']
+    r = collection_area(showers.energy, predictions.energy, bins=10, impact=270 * u.m)
+    area = r[0]
     assert area.si.unit == u.m**2
 
 
 def test_irf_writing(showers, predictions, tmpdir):
-    predictions['energy'] = predictions['MCorsikaEvtHeader.fTotalEnergy']
+    predictions['energy'] = predictions['corsika_evt_header_total_energy']
 
-    r = collection_area(showers.energy, predictions.energy, bins=10, impact=270*u.m,)
-    area, bin_center, bin_width, lower_conf, upper_conf  = r
+    r = collection_area(showers.energy, predictions.energy, bins=10, impact=270 * u.m,)
+    area, bin_center, bin_width, lower_conf, upper_conf = r
 
     t = collection_area_to_irf_table(area, bin_center, bin_width)
 
     assert len(t) == 1
-    
-if __name__ == '__main__':
-    test_irf_writing(showers(), predictions())
