8057b8a7404f6622a1de598287350bf005696a9e
Kai B <kai.bruegge@tu-dortmund.de>
2018-03-06 16:25:54 +0100

    more boilerplate useless crap to adhere to that weird format

---
+++ irf/oga.py
@@ -0,0 +1,194 @@
+import fact.io
+import astropy.units as u
+from astropy.io import fits
+from pandas.api.types import is_datetime64_any_dtype
+import pandas as pd
+from astropy.table import Table
+from astropy.time import Time
+import numpy as np
+
+MJDREF = 55835  # MJD sometime near FACT's first light. 2011-10-01T00:00:00 UTC
+
+
+def observation_id(night, run):
+    obs_id = (night * 1E3 + run).values
+    return obs_id.astype(int)
+
+
+def file_names_from_runs(runs):
+    run_ids = runs.run_id.values.astype(str)
+    nights = runs.night.values.astype(str)
+    names = [f'{n}_{r}_dl3.fits' for n, r in zip(nights, run_ids)]
+    return names
+
+
+
+def create_primary_hdu():
+    header = fits.Header()
+
+    header['OBSERVER'] = 'The non-insane FACT guys '
+    header['COMMENT'] = 'FACT EventList. Very preliminary'
+    header['COMMENT'] = 'See https://gamma-astro-data-formats.readthedocs.io/en/latest/'
+    header['COMMENT'] = 'This file was created by https://github.com/fact-project/irf'
+    header['COMMENT'] = 'See our full analysis here https://github.com/fact-project/open_crab_sample_analysis'
+
+    now = Time.now().iso
+    header['COMMENT'] = f'This file was created on {now}'
+
+    return fits.PrimaryHDU(header=header)
+
+
+def create_dl3_hdu(dl3):
+    '''
+    Takes a pandas dataframe which contains the DL3 Information and creates an hdu
+    accroding to the standard found here:
+    http://gamma-astro-data-formats.readthedocs.io/en/latest/events/events.html
+    return a fits hdu object
+    '''
+    # the format expects to see some event id. I just take the index
+    event_id = dl3.index
+
+    # convert from hour angles to deg
+    ra = dl3.ra_prediction.values * u.hourangle
+    ra = ra.to('deg')
+
+    dec = dl3.dec_prediction.values * u.deg
+
+    # convert to TeV energy
+    energy = dl3.gamma_energy_prediction.values * u.GeV
+    energy = energy.to('TeV')
+
+    # convert to timestamps. older pyfact versions don't do this automagically
+    if not is_datetime64_any_dtype(dl3.timestamp):
+        dl3.timestamp = pd.to_datetime(dl3.timestamp, infer_datetime_format=True)
+
+    time = Time(dl3.timestamp.asobject, location=fact.instrument.constants.LOCATION)
+    time = time - Time(MJDREF, scale='utc', format='mjd')
+
+    t = Table({'EVENT_ID': event_id, 'ENERGY': energy, 'DEC': dec, 'RA': ra, 'TIME': time.to('s')})
+    hdu = fits.table_to_hdu(t)
+    add_time_information_to_hdu(hdu)
+    hdu.header['EUNIT'] = 'TeV'
+    hdu.header['EXTNAME'] = 'EVENTS'
+    hdu.header['CREATOR'] = 'FACT IRF'
+    hdu.header['TELESCOP'] = 'FACT'
+    return hdu
+
+
+def create_index_hdu(runs):
+    obs_id = observation_id(runs.night, runs.run_id)
+    hdu_type = np.repeat('events', len(obs_id))
+    hdu_class = np.repeat('events', len(obs_id))
+    file_dir = np.repeat('./', len(obs_id))
+    file_name = file_names_from_runs(runs)
+    hdu_name = np.repeat('EVENTS', len(obs_id))
+
+    d = {
+        'OBS_ID': obs_id,
+        'HDU_TYPE': hdu_type,
+        'HDU_CLASS': hdu_class,
+        'FILE_DIR': file_dir,
+        'FILE_NAME': file_name,
+        'HDU_NAME': hdu_name,
+
+    }
+
+    hdu = fits.table_to_hdu(Table(d))
+    add_time_information_to_hdu(hdu)
+    hdu.header['EUNIT'] = 'TeV'
+    hdu.header['EXTNAME'] = 'EVENTS'
+    hdu.header['CREATOR'] = 'FACT IRF'
+    hdu.header['TELESCOP'] = 'FACT'
+    return hdu
+
+
+
+
+
+def create_observation_index_hdu(runs):
+    '''
+    Takes a pandas dataframe which contains the information about runs.
+    Take all the mandatory keywords found here:
+    http://gamma-astro-data-formats.readthedocs.io/en/latest/data_storage/obs_index/index.html
+    returns a fits hdu object
+    '''
+    obs_id = observation_id(runs.night, runs.run_id)
+    ra_pnt = runs.right_ascension.values * u.hourangle
+    ra_pnt = ra_pnt.to('deg')
+
+    dec_pnt = runs.declination.values * u.deg
+
+    zen_pnt = runs.zenith.values * u.deg
+    alt_pnt = (90 - runs.zenith.values) * u.deg
+    az_pnt = runs.azimuth.values * u.deg
+
+    if not is_datetime64_any_dtype(runs.run_start):
+        runs.run_start = pd.to_datetime(runs.run_start, infer_datetime_format=True)
+
+    if not is_datetime64_any_dtype(runs.run_stop):
+        runs.run_stop = pd.to_datetime(runs.run_stop, infer_datetime_format=True)
+
+    # in the format specified here
+    # http://gamma-astro-data-formats.readthedocs.io/en/latest/data_storage/obs_index/index.html
+    # ontime is the total observation time (including deadtime)
+    ontime = runs.run_stop - runs.run_start
+    ontime = ontime.dt.seconds.values * u.s
+    livetime = runs.ontime.values * u.s  # the runs table contains the thing FACT calls ontime
+    deadc = livetime / ontime
+    deadc = deadc.value
+
+    tstart = Time(runs.run_start.asobject, location=fact.instrument.constants.LOCATION)
+    tstart = tstart - Time(MJDREF, scale='utc', format='mjd')
+    tstart = tstart.to('s')
+
+    tstop = Time(runs.run_stop.asobject, location=fact.instrument.constants.LOCATION)
+    tstop = tstop - Time(MJDREF, scale='utc', format='mjd')
+    tstop = tstop.to('s')
+
+    date_obs = runs.run_start.dt.strftime('%Y-%m-%d').values.astype(np.str)
+    date_end = runs.run_stop.dt.strftime('%Y-%m-%d').values.astype(np.str)
+
+    time_obs = runs.run_start.dt.strftime('%H:%M:%S').values.astype(np.str)
+    time_end = runs.run_stop.dt.strftime('%H:%M:%S').values.astype(np.str)
+
+    n_tels = np.ones_like(obs_id)
+    tellist = np.ones_like(obs_id).astype(np.str)
+    quality = np.zeros_like(obs_id)
+
+    d = {
+        'OBS_ID': obs_id,
+        'RA_PNT': ra_pnt,
+        'DEC_PNT': dec_pnt,
+        'ZEN_PNT': zen_pnt,
+        'ALT_PNT': alt_pnt,
+        'AZ_PNT': az_pnt,
+        'ONTIME': ontime,
+        'LIVETIME': livetime,
+        'DEADC': deadc,
+        'TSTART': tstart,
+        'TSTOP': tstop,
+        'DATE-OBS': date_obs,
+        'TIME-OBS': time_obs,
+        'DATE-END': date_end,
+        'TIME-END': time_end,
+        'N_TELS': n_tels,
+        'TELLIST': tellist,
+        'QUALITY': quality,
+    }
+
+    hdu = fits.table_to_hdu(Table(d))
+    hdu.header['EUNIT'] = 'TeV'
+    hdu.header['EXTNAME'] = 'OBS_INDEX'
+    add_time_information_to_hdu(hdu)
+    return hdu
+
+
+def add_time_information_to_hdu(hdu):
+    hdu_header = hdu.header
+    hdu_header['MJDREFI'] = MJDREF
+    hdu_header['MJDREFF'] = 0
+    hdu_header['TIMEREF'] = 'local'
+    hdu_header['TIMESYS'] = 'utc'
+    hdu_header['GEOLON'] = -17.890701
+    hdu_header['GEOLAT'] = 28.761795
+    hdu_header['ALTITUDE'] = 2199.4
--- irf/scripts/fact_dl3_to_fits.py
+++ irf/scripts/fact_dl3_to_fits.py
@@ -1,13 +1,10 @@
 import fact.io
-import astropy.units as u
+from irf import oga
 import click
+import os
 from astropy.io import fits
-from pandas.api.types import is_datetime64_any_dtype
+from tqdm import tqdm
 import pandas as pd
-from astropy.table import Table
-from astropy.time import Time
-
-MJDREF = 55835  # MJD sometime near FACT's first light. 2011-10-01T00:00:00 UTC
 
 
 @click.command()
@@ -16,78 +13,52 @@ MJDREF = 55835  # MJD sometime near FACT's first light. 2011-10-01T00:00:00 UTC
     type=click.Path(file_okay=True, dir_okay=False),
 )
 @click.argument(
-    'output_path',
-    type=click.Path(file_okay=True, dir_okay=False, exists=False),
+    'output_directory',
+    type=click.Path(file_okay=False, dir_okay=True),
 )
 @click.option(
     '-t', '--threshold',
     default=0.85,
     help='Prediction threshold to apply.',
 )
-def main(input_path, output_path, threshold):
+def main(input_path, output_directory, threshold):
     dl3 = fact.io.read_h5py(input_path, key='events')
     dl3 = dl3.query(f'gamma_prediction >= {threshold}').copy()
 
-    primary_hdu = create_primary_hdu()
-    event_hdu = dl3_hdu_from_pandas(dl3)
+    runs = fact.io.read_h5py(input_path, key='runs')
 
-    hdu_header = event_hdu.header
-    hdu_header['MJDREFI'] = MJDREF
-    hdu_header['MJDREF'] = 0
-    hdu_header['TIMEREF'] = 'local'
-    hdu_header['TIMESYS'] = 'utc'
-    hdu_header['GEOLON'] = -17.890701
-    hdu_header['GEOLAT'] = 28.761795
-    hdu_header['ALTITUDE'] = 2199.4
-    hdu_header['EUNIT'] = 'TeV'
-    hdu_header['EXTNAME'] = 'EVENTS'
-    hdu_header['PRED_THR'] = threshold
+    os.makedirs(output_directory, exist_ok=True)
 
-    hdulist = fits.HDUList([primary_hdu, event_hdu])
-    hdulist.writeto(output_path, overwrite=True)
+    primary_hdu = oga.create_primary_hdu()
+    index_hdu = oga.create_observation_index_hdu(runs)
+    hdulist = fits.HDUList([primary_hdu, index_hdu])
+    hdulist.writeto(os.path.join(output_directory, 'obs-index.fits.gz'), overwrite=True)
 
 
-def create_primary_hdu():
-    header = fits.Header()
+    primary_hdu = oga.create_primary_hdu()
+    index_hdu = oga.create_index_hdu(runs)
+    hdulist = fits.HDUList([primary_hdu, index_hdu])
+    hdulist.writeto(os.path.join(output_directory, 'hdu-index.fits.gz'), overwrite=True)
 
-    header['OBSERVER'] = 'The non-insane FACT guys '
-    header['COMMENT'] = 'FACT EventList. Very preliminary'
-    header['COMMENT'] = 'See https://gamma-astro-data-formats.readthedocs.io/en/latest/'
-    header['COMMENT'] = 'This file was created by https://github.com/fact-project/irf'
-    header['COMMENT'] = 'See our full analysis here https://github.com/fact-project/open_crab_sample_analysis'
+    # import IPython; IPython.embed()
+    obs_ids = []
+    for n, g in tqdm(dl3.groupby(['night', 'run_id'])):
 
-    now = Time.now().iso
-    header['COMMENT'] = f'This file created {now}'
+        obs_ids.append(int(n[0] * 1E3 + n[1]))
 
-    return fits.PrimaryHDU(header=header)
+        run_data = g.copy()
+        oga.create_dl3_hdu(run_data)
+        primary_hdu = oga.create_primary_hdu()
+        event_hdu = oga.create_dl3_hdu(run_data)
+        hdulist = fits.HDUList([primary_hdu, event_hdu])
+        fname = f'{n[0]}_{n[1]}_dl3.fits'
+        hdulist.writeto(os.path.join(output_directory, fname), overwrite=True)
 
-
-
-
-def dl3_hdu_from_pandas(dl3):
-    # the format expects to see some event id. I just take the index
-    event_id = dl3.index
-
-    # convert from hour angles to deg
-    ra = dl3.ra_prediction.values * u.hourangle
-    ra = ra.to('deg')
-
-    dec = dl3.dec_prediction.values * u.deg
-
-    # convert to TeV energy
-    energy = dl3.gamma_energy_prediction.values * u.GeV
-    energy = energy.to('TeV')
-
-    # convert to timestamps. older pyfact versions don't do this automagically
-    if not is_datetime64_any_dtype(dl3.timestamp):
-        dl3.timestamp = pd.to_datetime(dl3.timestamp, infer_datetime_format=True)
-
-    time = Time(dl3.timestamp.asobject, location=fact.instrument.constants.LOCATION)
-    time = time - Time(MJDREF, scale='utc', format='mjd')
-
-    t = Table({'EVENT_ID': event_id, 'ENERGY': energy, 'DEC': dec, 'RA': ra, 'TIME': time.to('s')})
-    return fits.table_to_hdu(t)
-
+    pd.DataFrame({'OBS_ID': obs_ids}).to_csv(
+        os.path.join(output_directory, 'observations.csv'),
+        header=True,
+        index=False
+    )
 
 
 
