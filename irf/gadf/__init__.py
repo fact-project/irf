98bf89dbeed5e47bd44c920c59e7f31016440a3d
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2019-03-01 15:58:11 +0100

    add module docstring

--- irf/gadf/__init__.py
+++ irf/gadf/__init__.py
@@ -1,3 +1,11 @@
+'''
+This module contains some helpful methods tp create insturmetn response functions and 
+eventlists according to the open gamma-ray astro data format (gadf)
+
+See  https://gamma-astro-data-formats.readthedocs.io/en/latest/ for some specs. 
+
+'''
+
 from . import time
 from . import response
 from . import hdus
