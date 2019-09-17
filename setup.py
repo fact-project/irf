434827fdbbf7ddf7ce09618aa945515616c34c14
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-09-17 17:04:23 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='irf',
-    version='0.4.0',
+    version='0.4.1',
     description='Functions to do instrument response functions for FACT',
     url='http://github.com/fact-project/irf',
     author='Kai Brügge, Maximilian Nöthe',
